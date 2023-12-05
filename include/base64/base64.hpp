bbfc871bd1407f54b612e83908c3e55a863d5c6c
htmlgod <ivannevtrilov212@gmail.com>
2023-12-06 01:49:20 +0300

    base64::decode

--- include/base64/base64.hpp
+++ include/base64/base64.hpp
@@ -1,21 +1,49 @@
 #pragma once
 #include <cassert>
 #include <string_view>
+#include <bitset>
 
 namespace {
 constexpr std::string_view BASE64_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 constexpr auto PADDING = '=';
 
-std::string decode_quadruplet(std::string in) {
-    // TODO
-}
-
 struct base64_buf {
     base64_buf() : buf(0) {}
     uint32_t buf : 24;
 
 };
+
+int get_decoded_index(char in) {
+    if (std::isupper(in)) 
+        return static_cast<int>(in) - 65;
+    if (std::islower(in)) 
+        return static_cast<int>(in) - 97 + 26;
+    if (std::isdigit(in)) 
+        return static_cast<int>(in) - 48 + 52;
+    if (in == '+')
+        return 62;
+    return 63;
+}
+
+std::string decode_quadruplet(std::string in) {
+    base64_buf buf;
+    char end = -1;
+    size_t offset = in.size() - 1;
+    for (auto ch : in) {
+        if (ch == PADDING) {
+            end++;
+            continue;
+        }
+        buf.buf |= (get_decoded_index(ch) << 6*offset--);
+    }
+    int32_t tmp = buf.buf;
+    char* reader = reinterpret_cast<char*>(&tmp);
+    std::string result = "";
+    for (char i = 2; i > end; --i)
+        result.push_back(*(reader+i));
+    return result;
+}
 
 std::string encode_triplet(std::string in) {
     if (in.empty())
@@ -55,9 +83,14 @@ std::string encode(std::string in) {
 }
 
 std::string decode(std::string in) {
-    // TODO
+    if (in.empty())
+        return in;
     std::string result = "";
 
+    size_t quadruplets = in.size() / 4;
+    for (size_t i = 0; i < quadruplets; ++i) {
+        result += decode_quadruplet(in.substr(i*4, 4));
+    }
     return result;
 }
 
